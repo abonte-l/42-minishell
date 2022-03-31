6489e2848805339a7113aecfd8efd45001f04a78
abonte-l <abonte-l@student.42.fr>
2022-03-31 15:19:33 +0200

    double linked list libft

--- libft/inc/libft.h
+++ libft/inc/libft.h
@@ -20,10 +20,48 @@
 # define SIZE_OF_STRING 50 
 # define SIZE_OF_DELIM 5
 
+/*
+** defining node of the linked list
+*/
+typedef struct		s_node
+{
+	char			*data;
+	int				index;
+	struct s_node 	*next;
+	struct s_node	*prev;
+}					t_node;
+
+/*
+** defining a double linked list
+*/
+typedef struct		s_dlst
+{
+	int				length;
+	struct s_node 	*tail;
+	struct s_node	*head;
+}					t_dlst;
+
+/*
+** double linked list functions
+*/
+t_dlst	*dlist_new(void);
+t_dlst	*dlist_add_end(t_dlst *list, char *data);
+t_dlst	*dlist_add_start(t_dlst *list, char *data);
+void 	dlist_delete(t_dlst **list);
+void	new_node_end(t_dlst *list, t_node *new_node, char *data);
+void	new_node_start(t_dlst *list, t_node *new_node, char *data);
+
+/*
+** memory functions
+*/
 void	ft_bzero(void *s, size_t n);
 void	*ft_calloc(size_t count, size_t size);
-void		*ft_realloc(void *ptr, size_t size);
+void	*ft_realloc(void *ptr, size_t size);
 void	*ft_memcpy(void *dst, const void *src, size_t n);
+
+/*
+** string functions
+*/
 char	*ft_strdup(const char *s1);
 char 	*ft_strtok(char *str, char *delim);
 char	*ft_strcat(char *dest, const char *src);
