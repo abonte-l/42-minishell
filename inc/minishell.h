f56ff6421829eea31c37038de76deef98f01d55c
abonte-l <abonte-l@student.42.fr>
2022-03-31 19:19:50 +0200

    add includes Makefile

--- inc/minishell.h
+++ inc/minishell.h
@@ -1,7 +1,7 @@
 #ifndef _MINISHELL_H
 # define _MINISHELL_H
 
-//# include <pwd.h>
+# include <pwd.h>
 # include <signal.h>
 # include <stdbool.h>
 # include <stdio.h>
@@ -9,8 +9,8 @@
 # include <string.h>
 # include <unistd.h>
 # include <sys/wait.h>
-//# include <sys/types.h>
-//# include <linux/limits.h>
+# include <sys/types.h>
+# include <linux/limits.h>
 # include <readline/readline.h>
 # include <readline/history.h>
 # include "../libft/inc/libft.h"
@@ -19,12 +19,16 @@
 # define ERROR -1
 # define ERROR_TEXT	"Error\n"
 
+# define TRUE 1
+# define FALSE 0
+
+# define STDIN 0
+# define STDOUT 1
+# define STDERR 2
+
 # define INT_MAX 2147483647
 # define INT_MIN -2147483648
 # define INT_ERROR 2147483648
-
-# define TRUE 1
-# define FALSE 0
 
 # define SPACE ' '
 # define TAB '\t'
