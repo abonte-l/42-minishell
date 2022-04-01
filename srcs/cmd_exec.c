58c55eb0e2e42fd0be108c504afd447a2618a91c
abonte-l <abonte-l@student.42.fr>
2022-04-01 11:29:19 +0200

    modif comments

--- srcs/cmd_exec.c
+++ srcs/cmd_exec.c
@@ -6,7 +6,7 @@
 /*   By: abonte-l <abonte-l@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/29 13:18:24 by abonte-l          #+#    #+#             */
-/*   Updated: 2022/04/01 11:19:31 by abonte-l         ###   ########.fr       */
+/*   Updated: 2022/04/01 11:29:11 by abonte-l         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,11 +15,11 @@
 /* ************************************************************************** */
 /*                             CMD_EXEC FUNCTION                              */
 /*                                                                            */
-/* lines 33-35 : forking parent and child processes                        */
+/* lines 33-35 : forking parent and child processes                           */
 /* lines 36-46 : If the fork was successful the parent process                */
-/* 				waits for the child (process fork)                            */
+/*              waits for the child (process fork)                            */
 /* lines 38-39 : block the parent process until the child terminates          */
-/* 				then kill the child process 								  */
+/*              then kill the child process                                   */
 /* lines 43-46 : Child process execute command or exit if execve fails        */
 /*                                                                            */
 /* ************************************************************************** */
